aeb2aa19e015ecfc74ef0cbe57fa73ee9daa374c
David Nadeau <nadeaud.d@gmail.com>
2018-05-23 11:53:23 -0700

    Lesson2 Completed!

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+	public GameObject asteroidExplosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag ("Boundary")) {
+			return;
+		}
+		if (other.CompareTag("Player")){
+			Debug.Log ("Hit Player");
+			Instantiate (playerExplosion, transform.position,transform.rotation);
+		}
+		Instantiate (asteroidExplosion, transform.position,transform.rotation);
+
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+
+	}
+}
