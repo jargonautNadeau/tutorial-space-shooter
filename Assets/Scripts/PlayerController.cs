99d51c54ec44fc4dd53f1d960ea159c9e3f6b3d5
David Nadeau <nadeaud.d@gmail.com>
2018-05-22 17:18:58 -0700

    Add Project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin,xMax,zMin,zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 10.0f;
+	public float tilt;
+	public Boundary boundary;
+	public float shotCooldown = 0.5f;
+	private float nextShot = 0.0f;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	void Update(){
+		if (Input.GetButton ("Fire1") && Time.time > nextShot) {
+			nextShot = Time.time + shotCooldown;
+			Instantiate (shot,shotSpawn.position,shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody> ().velocity = movement * speed;
+
+
+		GetComponent<Rigidbody> ().position = new Vector3 (
+			Mathf.Clamp(GetComponent<Rigidbody> ().position.x,boundary.xMin,boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp(GetComponent<Rigidbody> ().position.z,boundary.zMin,boundary.zMax));
+
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody> ().velocity.x * -tilt);
+			
+	}
+}
